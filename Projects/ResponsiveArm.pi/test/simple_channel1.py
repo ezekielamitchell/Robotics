5d390e7c0b435ee158e8ef5ab219c8ac808ce5c6
Ezekiel A. Mitchell <Ezekielam@icloud.com>
2024-10-15 16:54:52 -0700

    docker init

--- Projects/ResponsiveArm.pi/test/simple_channel1.py
+++ Projects/ResponsiveArm.pi/test/simple_channel1.py
@@ -5,6 +5,7 @@
 import time    #https://docs.python.org/fr/3/library/time.html
 from adafruit_servokit import ServoKit    #https://circuitpython.readthedocs.io/projects/servokit/en/latest/
 
+
 #Constants
 nbPCAServo=16 
 
@@ -21,35 +22,26 @@ pca = ServoKit(channels=16)
 def init():
 
     pca.servo[8].angle=90
+    pca.servo[9].angle=90
+    time.sleep(0.01)
 
-    # for i in range(nbPCAServo):
-    #     pca.servo[i].set_pulse_width_range(MIN_IMP[i] , MAX_IMP[i])
-
-
-# function main 
-def main():
-
-    pcaScenario();
-
-
-# function pcaScenario 
-def pcaScenario():
-    """Scenario to test servo"""
     for i in range(nbPCAServo):
         for j in range(MIN_ANG[i],MAX_ANG[i],1):
             print("Send angle {} to Servo {}".format(j,i))
-            pca.servo[i].angle = j
+            pca.servo[0].angle = j
+            pca.servo[4].angle = j
             time.sleep(0.01)
         for j in range(MAX_ANG[i],MIN_ANG[i],-1):
             print("Send angle {} to Servo {}".format(j,i))
-            pca.servo[i].angle = j
+            pca.servo[0].angle = j
+            pca.servo[4].angle = j
             time.sleep(0.01)
-        pca.servo[i].angle=None #disable channel
+        pca.servo[0].angle=None #disable channel
         time.sleep(0.5)
-
 
 
 
 if __name__ == '__main__':
     init()
+    
     # main()
