4148e22810728bfe6419a9ecd739c52a8c7e1470
Ezekiel A. Mitchell <ezekielam@icloud.com>
2024-12-10 19:02:33 -0800

    ros2 run pkg1 python_node

--- Reference/ROS2/ros2_ws/src/pkg1/pkg1/node1.py
+++ Reference/ROS2/ros2_ws/src/pkg1/pkg1/node1.py
@@ -3,11 +3,23 @@
 import rclpy
 from rclpy.node import Node
 
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("python_test")
+        self.counter_ = 0
+        self.get_logger().info("Hello @__init__")
+        self.create_timer(0.5, self.timer_callback)
+
+    def timer_callback(self):
+        self.counter_ += 1
+        self.get_logger().info("Hello @timer_callback : counter = " + str(self.counter_))
+
+
 def main(args=None):
     rclpy.init(args=args)
 
-    node = Node("python_test")
-    node.get_logger().info("Hello, Ezekiel -- FROM ROS2")
+    node = MyNode()
 
     rclpy.spin(node=node)
     rclpy.shutdown()
