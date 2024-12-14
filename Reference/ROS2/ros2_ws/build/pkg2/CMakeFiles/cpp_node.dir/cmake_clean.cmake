6a1d80965e6a163c98499a135c8a67bec148c6a6
Ezekiel A. Mitchell <ezekielam@icloud.com>
2024-12-14 10:19:33 -0800

    ros2 node creation

---
+++ Reference/ROS2/ros2_ws/build/pkg2/CMakeFiles/cpp_node.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_node.dir/link.d"
+  "CMakeFiles/cpp_node.dir/src/node1.cpp.o"
+  "CMakeFiles/cpp_node.dir/src/node1.cpp.o.d"
+  "cpp_node"
+  "cpp_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Reference/ROS2/ros2_ws/build/pkg2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/endr/Developer/Robotics/Reference/ROS2/ros2_ws/src/pkg2
+# Build directory: /home/endr/Developer/Robotics/Reference/ROS2/ros2_ws/build/pkg2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
