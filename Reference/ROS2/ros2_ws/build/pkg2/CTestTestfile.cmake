645e332a439064a79061a05a4cc7351f2fa13ac5
Ezekiel A. Mitchell <ezekielam@icloud.com>
2025-06-26 18:54:36 -0700

    cleanup

--- Reference/ROS2/ros2_ws/build/pkg2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/endr/Developer/Robotics/Reference/ROS2/ros2_ws/src/pkg2
-# Build directory: /home/endr/Developer/Robotics/Reference/ROS2/ros2_ws/build/pkg2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Reference/ROS2/ros2_ws/src/build/pkg1/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/endr/Developer/Robotics/Reference/ROS2/ros2_ws/src/install/pkg1'
